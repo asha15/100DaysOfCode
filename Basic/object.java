446b8701d42fad453724c12f828433fbee34c4ed
Asha Sulaiman <165079T@uom.lk>
2019-05-06 22:35:28 +0530

    object

---
+++ Basic/object.java
@@ -0,0 +1,17 @@
+<html>
+
+	<body>
+		
+		<h2>Object</h2>
+		<p id="word"></p>
+
+		<script>
+
+			var car={type="Bens": model="300": colour="white"};
+			document.getElementById.inner.HTML= "The car type is" + type + car.type;
+	
+		</script>
+
+	</body>
+
+</html>
